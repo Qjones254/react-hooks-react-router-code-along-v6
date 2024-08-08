d25f28430bd8cefb84ece3beee10aac3c30943d3
quincy mwangi <mwangiquincy77@gmail.com>
2024-08-08 11:00:54 +0300

    updated paths

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home";
 import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import About from "./pages/About";
+import Login from "./pages/Login";
 
-const router= createBrowserRouter([{
-    path:'/',
-    element:<Home/>
-}
-
+const router= createBrowserRouter([
+    {
+        path:'/',
+        element:<Home/>
+    },
+    {
+        path:'/about',
+        element:<About/>
+    },
+    {
+        path:'/login',
+        element:<Login/>
+    }
 ])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
